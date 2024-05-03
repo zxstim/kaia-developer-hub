6030fbf5950f99eeef365b27ebfd64fea845b63b
ZxStim <thang.victor.luong@gmail.com>
2024-05-03 15:47:06 +0700

    added sidebar nav and page nav

---
+++ app/bootcamp/data.tsx
@@ -0,0 +1,23 @@
+export const dappBootcampsCategoryNavData = [
+  {
+    id: 1,
+    title: 'English',
+    slug: '/bootcamp/en',
+    parent: null,
+  },
+  {
+    id: 2,
+    title: 'Solidity',
+    slug: '/library/dapp-bootcamps/vi/solidity',
+    parent: null
+  },
+]
+
+export const dappBootcampsItemNavData = [
+  {
+    id: 3,
+    title: 'Blockchain and Kaia Overview',
+    slug: '/bootcamp/en/solidity/intro/blockchain-and-kaia-overview',
+    parent: 2
+  }
+]
--- app/bootcamp/layout.tsx
+++ app/bootcamp/layout.tsx
@@ -1,10 +1,16 @@
-import Link from 'next/link'
+
+import { dappBootcampsCategoryNavData, dappBootcampsItemNavData } from './data'
+import SidebarNav from '@/components/sidebar-nav'
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
+
   // Create any shared layout or styles here
   return (
-    <div className="flex flex-col text-left w-full gap-4">
-      {children}
+    <div className="grid grid-cols-1 md:grid-cols-[320px_minmax(768px,_1fr)] text-left gap-8 justify-center">
+      <SidebarNav categories={dappBootcampsCategoryNavData} items={dappBootcampsItemNavData} />
+      <div className='flex flex-col gap-8'>
+        {children}
+      </div>
     </div>
   )
 }

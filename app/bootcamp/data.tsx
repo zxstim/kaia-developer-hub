0f412c280dbced891fa5c0c415f4552c8db6d631
ZxStim <thang.victor.luong@gmail.com>
2024-05-09 16:03:00 +0700

    added more contents

--- app/bootcamp/data.tsx
+++ app/bootcamp/data.tsx
@@ -31,5 +31,35 @@ export const dappBootcampsItemNavData = [
     title: 'Contract structure',
     slug: '/bootcamp/en/solidity/contract-structure',
     parent: 2
+  },
+  {
+    id: 5,
+    title: 'Getting started',
+    slug: '/bootcamp/en/solidity/getting-started',
+    parent: 2
+  },
+  {
+    id: 6,
+    title: 'Value types',
+    slug: '/bootcamp/en/solidity/value-types',
+    parent: 2
+  },
+  {
+    id: 7,
+    title: 'Reference types',
+    slug: '/bootcamp/en/solidity/reference-types',
+    parent: 2
+  },
+  {
+    id: 8,
+    title: 'Mapping types',
+    slug: '/bootcamp/en/solidity/mapping-types',
+    parent: 2
+  },
+  {
+    id: 9,
+    title: 'Basic solidity exercise',
+    slug: '/bootcamp/en/solidity/basic-solidity-exercise',
+    parent: 2
   }
 ]
